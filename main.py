08875c7b724f653b22fe194596ab5107c1e6516c
butorisa <buzzbuzzsatellites@gmail.com>
2020-05-30 12:06:10 +0900

    2020/05/29 オフィス画面作成、オブジェクトが流れるアニメを追加

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pyxel
 from enum import Enum, auto
+from random import randint
 
 
 class GameMode(Enum):
     """ 画面一覧 """
-    # 町
     Town = auto()
-    # 家の中
-    Home = auto()
+    Office = auto()
 
 
 class PrettyHopper:
@@ -16,8 +15,13 @@ class PrettyHopper:
         """ 初期化 """
         pyxel.init(128, 128, caption='pretty hopper')
         pyxel.load('image/pretty-hopper.pyxres')
+        self.start_frame = pyxel.frame_count
         self.player = Player()
-        self.home = MyHome()
+        self.office = Office()
+        # オブジェクト
+        self.duke = [(i * 60, randint(0, 104)) for i in range(4)]
+        self.python = [(i * 60, randint(0, 104)) for i in range(4)]
+        self.ruby = [(i * 60, randint(0, 104)) for i in range(4)]
         # 町からスタート
         self.game_mode = GameMode.Town
         # 画像の点滅表示に使用
@@ -31,32 +35,53 @@ class PrettyHopper:
             pyxel.quit()
 
         self.player.move()
-        self.switch_active(self.player, self.home)
+
+        if self.game_mode == GameMode.Town:
+            self.switch_active(self.player, self.office)
+
+        if self.game_mode == GameMode.Office:
+            for i, v in enumerate(self.duke):
+                self.duke[i] = self.update_object(*v)
+            for i, v in enumerate(self.python):
+                self.python[i] = self.update_object(*v)
+            for i, v in enumerate(self.ruby):
+                self.ruby[i] = self.update_object(*v)
 
     def draw(self):
         """ 画面描画 """
         if self.game_mode == GameMode.Town:
             self.draw_town()
-        elif self.game_mode == GameMode.Home:
+        elif self.game_mode == GameMode.Office:
             self.draw_loading()
-            # self.draw_home()
+            # ローディング画面表示終了
+            if (pyxel.frame_count - self.start_frame) > 90:
+                self.draw_office()
 
     def draw_town(self):
         """ 町を描画 """
         # 背景
         pyxel.cls(7)
         pyxel.bltm(0, 0, 0, 0, 0, 16, 16)
-        # 家
-        pyxel.blt(self.home.position[0], self.home.position[1], 0, 16, 0, 16, 16, 5)
-        # うさぎ
-        pyxel.blt(40, 80, 0, 48, 0, 16, 16, 5)
+        # オフィス
+        pyxel.blt(self.office.position[0], self.office.position[1], 0, 16, 0, 16, 16, 5)
         # プレイヤー
         if self.player.active:
             pyxel.blt(self.player.position[0], self.player.position[1], 0, 0, 0, 16, 16, 5)
 
-    def draw_home(self):
-        """ 家の中を描画 """
-        pass
+    def draw_office(self):
+        """ ゲーム画面を描画 """
+        pyxel.cls(7)
+        pyxel.bltm(0, 0, 0, 48, 0, 16, 16)
+
+        # オブジェクト
+        for x, y in self.duke:
+            pyxel.blt(x, y, 0, 32, 80, 16, 16, 5)
+        for x, y in self.python:
+            pyxel.blt(x, y, 0, 48, 80, 16, 16, 5)
+        for x, y in self.ruby:
+            pyxel.blt(x, y, 0, 0, 96, 16, 16, 5)
+        # プレイヤー
+        pyxel.blt(self.player.position[0], self.player.position[1], 0, 0, 0, 16, 16, 5)
 
     def draw_loading(self):
         """ ロード画面を描画 """
@@ -80,11 +105,30 @@ class PrettyHopper:
                 and abs(obj.position[1] - target.position[1]) < 10:
             obj.active = False
             # ロード画面
-            self.game_mode = GameMode.Home
-        else:
-            obj.active = True
-            # ゲーム画面
-            self.game_mode = GameMode.Town
+            self.game_mode = GameMode.Office
+            # フレーム数を保持
+            self.start_frame = pyxel.frame_count
+        # else:
+        #     obj.active = True
+        #     # ゲーム画面
+        #     self.game_mode = GameMode.Town
+
+    def update_object(self, x, y):
+        """ オブジェクトのアニメーション """
+        if abs(x - self.player.position[0]) < 12 and abs(y - self.player.position[1]) < 12:
+            # is_active = False
+            # self.score += 100
+            # self.player_vy = min(self.player_vy, -8)
+            pyxel.play(3, 4)
+
+        x -= 2
+
+        if x < -40:
+            x += 240
+            y = randint(0, 104)
+            # is_active = True
+
+        return (x, y)
 
 
 class Player:
@@ -105,10 +149,10 @@ class Player:
             self.position[1] += 10
 
 
-class MyHome:
+class Office:
     """ 家オブジェクト """
     def __init__(self):
-        self.position = [88, 72]
+        self.position = [96, 64]
 
 
 PrettyHopper()
